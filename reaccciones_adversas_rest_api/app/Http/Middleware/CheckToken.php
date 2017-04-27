a4e8ad3307e160ab5e3b74294aac497a082668d8
mrls94 <sebasmrls94@gmail.com>
2017-04-27 20:02:28 +0000

    Add token expiration

--- reaccciones_adversas_rest_api/app/Http/Middleware/CheckToken.php
+++ reaccciones_adversas_rest_api/app/Http/Middleware/CheckToken.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\Usuario;
 
 class CheckToken
 {
@@ -13,11 +14,36 @@ class CheckToken
      * @param  \Closure  $next
      * @return mixed
      */
+     
+    const MINUTES_TOKEN_EXPIRATION = 30;
+     
     public function handle($request, Closure $next)
     {
+        $answer = new \stdClass();
+        $now_date = date(DATE_ATOM);
+        
         if($request->header('token')){
-         return $next($request);   
+            $token = $request->header('token');
+            try{
+                $user = Usuario::where('token', $token)->firstOrFail();
+                $token_date_str = $user->token_generation_date;
+                $token_date = strtotime($token_date_str);
+                $now = strtotime($now_date);
+                $time_alive = round(abs($token_date - $now) / 60); //60 minutos en una hora :O
+                
+                if($time_alive < self::MINUTES_TOKEN_EXPIRATION){
+                    return $next($request);   
+                } else {
+                    $answer->error = "token expired";
+                    return response(json_encode($answer), 400);
+                }
+                
+            } catch (\Exception $e){
+                $answer->error = "in-valid token";
+                return response(json_encode($answer), 400);   
+            }
         }
-        return response("Un-authorized", 400);
+        $answer->error = "Un-authorized";
+        return response($answer, 400);
     }
 }
--- reaccciones_adversas_rest_api/app/Http/routes.php
+++ reaccciones_adversas_rest_api/app/Http/routes.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::post('login', ['uses' => 'UsuariosController@login']);
+
+//Por ahora el único endpoint sin middleware es login -- middleware => check_token para el resto
 Route::post('User/new', ['uses' => 'UsuariosController@create', 'middleware' => 'check_token']);
 Route::get('hello', ['uses' => 'UsuariosController@hello', 'middleware' => 'check_token']);
-Route::post('login', ['uses' => 'UsuariosController@login']);
